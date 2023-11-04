d3d4f9a159139f121e4179c0ee8561209015989a
PERicci <riccip@outlook.com>
2023-11-04 13:07:45 +0000

    answer changed level-400 a)

--- js1-wk1-eval/level-400.js
+++ js1-wk1-eval/level-400.js
@@ -13,7 +13,7 @@ confession = confession.replaceAll(secret,'x'.repeat(secret.length));
 // d) How any inputs do we pass to repeat when we call it?  How can you tell?
 // e) What kind of statement is on line 7?
 
-// a) 3
+// a) 2
 // b) Find all "cake" (variable secret) and replace for "xxxx" (x repeated 4 times due the secret.length)
 // c) 2 (first is the element to look for and the second one is the one to replace one with)
 // d) 1 (amount of repetitions)
