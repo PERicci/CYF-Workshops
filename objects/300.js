36805944c3a97541eca249e3966c9d6baca650c2
Pedro <riccip@outlook.com>
2023-12-09 15:30:26 +0000

    objects 300 done

--- objects/300.js
+++ objects/300.js
@@ -1,11 +1,16 @@
 // Predict and explain...
 // What will happen when this program is run?
+// false for both because in both objects there is not the key 'location'.
+// So, the location will be undefined e the includes cannot find undefined in cyfLocations
 // Check your prediction and explanation by running the program.
+// I didn`t notice that the last assertion has a ! operator. So, in this case, receiving false change to true.
+// I`ve solved the bug, and the last assertion is true because there is not Edinburgh in cyfLocations
 // Fix anything that needs fixing.
 
 function checkLivesNearCYF(person) {
     const cyfLocations = ["Birmingham", "Cape Town", "Glasgow", "London", "Manchester"];
-    return cyfLocations.includes(person.location);
+    // return cyfLocations.includes(person.location);
+    return cyfLocations.includes(person.city);
 }
 
 const mo = {
