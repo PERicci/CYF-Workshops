ac2318b45b8776695400c12ef4826f266e5f524a
Pedro <riccip@outlook.com>
2023-12-09 14:59:51 +0000

    objects 200.js done

--- objects/200.js
+++ objects/200.js
@@ -1,14 +1,19 @@
 // Predict and explain...
 // What will happen when this program is run?
+// There are bugs in lines 9 (the : is missing) and 10 (the sign inside the object is not =)
 // Check your prediction and explanation by running the program.
 // Fix anything that needs fixing.
 
 const person = {
     "name": "Jemima",
-    "location",
-    "id_number" = 9,
+    "location": "Glasgow",
+    "id_number": 9,
 };
 
+// after the fix
 console.assert(person.name === "Jemima");
+// pass
 console.assert(person.location === "Glasgow");
+// pass
 console.assert(person.id_number === 9);
+// pass
