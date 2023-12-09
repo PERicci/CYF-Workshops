88f04d1e88e98ac6f5ab9f30b747ef0d98f6e640
Pedro <riccip@outlook.com>
2023-12-09 15:49:20 +0000

    objects 600 done

--- objects/600.js
+++ objects/600.js
@@ -1,5 +1,8 @@
 // Predict and explain...
 // What will happen when this program is run?
+// It is not working properly as the 'field' parameter is not the same of the .field key.
+// It doesn't matter if we call the function with the field parameter equals to 'favourite_ice_cream'.
+// The function will search a key called 'field' inside the object.
 // Check your prediction and explanation by running the program.
 // Fix anything that needs fixing.
 
@@ -10,7 +13,8 @@ const person = {
 };
 
 function assertFieldEquals(object, field, targetValue) {
-    console.assert(object.field === targetValue);
+    // console.assert(object.field === targetValue);
+    console.assert(object[field] === targetValue);
 }
 
 assertFieldEquals(person, "favourite_ice_cream", "vanilla");
