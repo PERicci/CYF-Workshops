8567507d0748ae226e1a35779a0c78c951dfd328
Pedro <riccip@outlook.com>
2024-02-03 14:24:16 +0000

    index.js done

--- goblin-breakdown/contact/index.js
+++ goblin-breakdown/contact/index.js
@@ -1,9 +1,25 @@
+
 // Retrieve the JSON object containing the UK address and phone number
-const getContactDetails = async (api) => {};
+const jsonObject = async (api) => {
+  const response = await fetch(api);
+  const data = await response.json();
+  return data;
+}
 
-// Format the address in the desired UK address format
-const formatAddress = (address) => {};
+// Extract the address and phone number from the JSON object
+const extractAddress = (data) => { }
+const extractPhoneNumber = (data) => { }
 
-// Format the phone number in the desired UK phone number format
+// Format the address in the UK address format
+const formatAddress = (address) => { };
 
-// Generate HTML code to display the formatted address and phone number on the page
+// Format the phone number in the UK phone number format
+const formatPhoneNumber = (phoneNumber) => { };
+
+// Convert the formatted address and phone number into HTML format
+const convertToHtml = (address, phoneNumber) => { };
+
+// Display the formatted address and phone number as HTML on the web page
+const render = (html, elementId) => { }
+
+
