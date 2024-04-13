936148982bc2d85e60c8ac474c14265a3d32f4db
Pedro <riccip@outlook.com>
2024-04-13 11:38:19 +0100

    added all endpoints, implemented post and delete album

--- crud/server.js
+++ crud/server.js
@@ -2,6 +2,51 @@ const express = require("express");
 const albumsData = require("./albums.json");
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000. Ready to accept requests!");
+const port = 3000
+const endpoints = {
+  main: "/",
+  albums: "/albums",
+}
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}. Ready to accept requests!`);
 });
+
+//get routes
+
+app.get(endpoints.main, (req, res) => {
+  res.send(`Available endpoints: ${Object.values(endpoints).join(", ")}`);
+})
+
+app.get(endpoints.albums, (req, res) => {
+  res.send(albumsData);
+})
+
+app.get(`${endpoints.albums}/:albumId`, (req, res) => {
+  const albumId = req.params.albumId;
+  const album = albumsData.find(album => album.albumId === albumId);
+  album ? res.send(album) : res.status(404).send("Album not found");
+});
+
+// post routes
+
+app.post(endpoints.albums, (req, res) => {
+  const newAlbum = req.body;
+  albumsData.push(newAlbum);
+  res.send("Album added successfully!");
+})
+
+//delete routes
+
+app.delete(`${endpoints.albums}/:albumId`, (req, res) => {
+  const albumId = req.params.albumId;
+  const index = albumsData.findIndex(album => album.albumId === albumId);
+  if (index !== -1) {
+    albumsData.splice(index, 1);
+    res.send("Album deleted successfully!");
+  } else {
+    res.status(404).send("Album not found");
+  }
+})
